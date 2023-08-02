751a94aed9681dec2e44b05f7714b4a763eb17fa
Dima Levischenko <dimalevischenko@gmail.com>
2023-08-02 17:31:20 +0300

    added auto scale

--- view/content.php
+++ view/content.php
@@ -28,6 +28,10 @@ $order = wc_get_order( $wpPost->ID );
     <div  class="h-full flex flex-col mr-2 space-y-2">&nbsp;
         <a class="p-1 w-full bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 text-white" href="javascript:void(0);" onclick="drawData();" >Apply</a>
     </div>
+    <div  class="h-full flex flex-col mr-2 space-y-2">
+        &nbsp;
+        <a class="p-1 w-full bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 text-white" href="javascript:void(0);" onclick="autoScaleDrawData()">Auto Scale</a>
+    </div>
 </div>
 <div id="visualize-3d-pallet-root">Loading.</div>
 <script src="<?php echo URL_3D_PALLET_DIR_JS; ?>main.js" ></script>
@@ -72,40 +76,201 @@ $order = wc_get_order( $wpPost->ID );
             console.info('data:', data)
             console.info('products:', products)
             console.info('result:', window.lotus.result)
-            const elInfoBlock = document.querySelector('#result-row');
-            elInfoBlock.innerHTML = '';
-            window.lotus.result.forEach((item, idx)=>{
-                if (!item || idx < 1) {
-                    return;
-                }
-                if (typeof item === "string") {
-                    const [name, value] = item.split(':');
-                    elInfoBlock.innerHTML += `<p><strong>${name}:</strong>${value}</p>`;
-                }
-                if (typeof item === "object") {
+            applyResult(window.lotus.result);
 
-                    elInfoBlock.innerHTML += `<p><strong>${item[0]}</strong></p>`;
-                    const unpackedBoxes = [];
-                    item.slice(1).forEach(unpacked=>{
-                        const id = parseInt(unpacked.substr(0, 2));
-                        if (unpackedBoxes.indexOf(id) === -1) {
-                            const name = products[id].name;
-                            const size = unpacked.substr(2);
-                            unpackedBoxes.push(id);
-                            elInfoBlock.innerHTML += `<p>&nbsp&nbsp&nbsp<strong>${name}:</strong>${size}</p>`;
-                        }
-                    })
-                }
-                elInfoBlock.style.display = 'block';
-            });
         })
 
     }
 
-    document.addEventListener('DOMContentLoaded', function (){
+    function applyResult(result) {
+        const elInfoBlock = document.querySelector('#result-row');
+        elInfoBlock.innerHTML = '';
+        result.forEach((item, idx)=>{
+            if (!item || idx < 1) {
+                return;
+            }
+            if (typeof item === "string") {
+                const [name, value] = item.split(':');
+                elInfoBlock.innerHTML += `<p><strong>${name}:</strong>${value}</p>`;
+            }
+            if (typeof item === "object") {
 
+                elInfoBlock.innerHTML += `<p><strong>${item[0]}</strong></p>`;
+                const unpackedBoxes = [];
+                item.slice(1).forEach(unpacked=>{
+                    const id = parseInt(unpacked.substr(0, 2));
+                    if (unpackedBoxes.indexOf(id) === -1) {
+                        const name = products[id].name;
+                        const size = unpacked.substr(2);
+                        unpackedBoxes.push(id);
+                        elInfoBlock.innerHTML += `<p>&nbsp&nbsp&nbsp<strong>${name}:</strong>${size}</p>`;
+                    }
+                })
+            }
+            elInfoBlock.style.display = 'block';
+        });
+    }
+
+    document.addEventListener('DOMContentLoaded', function (){
         drawData();
     });
+
+
+    var isRunTime = false;
+    var intervalId = 0;
+    const results = [];
+    var lastSend = '';
+    var diffWidth = 0;
+    var sendWidth = 0;
+    var diffDepth = 0;
+    var sendDepth = 0;
+    var diffHeight = 0;
+    var sendHeight = 0;
+
+    var packedBoxes = 0;
+
+
+    var step = 1;
+    var minWidth = 0;
+    function autoScaleDrawData() {
+        clearInterval(intervalId);
+        console.info('Auto scale start.')
+        diffWidth = 0;
+        sendWidth = 0;
+        diffDepth = 0;
+        sendDepth = 0;
+        diffHeight = 0;
+        sendHeight = 0;
+        minWidth = 0;
+        packedBoxes = 0;
+        step = 1;
+
+
+        let depth = parseInt(document.querySelector('#depth').value)||100;
+        let width = parseInt(document.querySelector('#width').value)||120;
+        let height = parseInt(document.querySelector('#height').value)||220;
+
+        let data = '';
+        products.forEach((product, i)=>{
+            data += `\n${i}. ${product.d}, ${product.h}, ${product.w}, ${product.q}`;
+            if (minWidth < product.w) {
+                minWidth = product.w;
+            }
+        });
+
+        intervalId = setInterval(() => {
+
+            if (isRunTime) {
+                return;
+            }
+            isRunTime = true;
+
+
+
+            if (window.lotus.result) {
+                const result = window.lotus.result;
+                window.lotus.result = null;
+                const resultData = {
+                    result,
+                    size: result[1],
+                    volume: result[2].split(': ')[1],
+                    useVolume: result[3].split(': ')[1],
+                    lastSend,
+                };
+
+
+                const packed = parseInt(result[4].split(': ')[1]);
+                const total = parseInt(result[5].split(': ')[1]);
+                if (step===1) {
+                    if (packedBoxes <= packed) {
+                        packedBoxes = packed;
+                        diffWidth += Math.ceil(sendWidth / 2);
+                        diffDepth += Math.ceil(sendDepth / 2);
+                        results.push(resultData)
+                        applyResult(result);
+                    } else {
+                        console.info('next step')
+                        step++;
+                        diffWidth -= Math.ceil(diffWidth / 2);
+                        diffDepth -= Math.ceil(diffDepth / 2);
+                    }
+                }else {
+                    if (step===2) {
+                        console.info('step 2')
+                        if (packedBoxes <= packed) {
+                            diffWidth++;
+                            diffDepth++;
+                            results.push(resultData)
+                            applyResult(result);
+                        } else {
+                            console.info('next step')
+                            step++;
+                            diffWidth--;
+                            diffDepth--;
+                        }
+                    }else {
+                        if (step===3) {
+                            console.info('step 3')
+                            if (packedBoxes <= packed) {
+                                diffDepth++;
+                                results.push(resultData)
+                                applyResult(result);
+                            }else {
+                                console.info('next step')
+                                step++;
+                                diffDepth--;
+                            }
+                        }else {
+                            if (step===4) {
+                                console.info('step 4')
+                                if (packedBoxes <= packed) {
+                                    diffHeight+= 5;
+                                    results.push(resultData)
+                                    applyResult(result);
+                                } else {
+                                    step++;
+                                    diffHeight-=5;
+                                }
+                            }else {
+                                console.info('step 5')
+                                if (packedBoxes <= packed) {
+                                    diffHeight++;
+                                    results.push(resultData)
+                                    applyResult(result);
+                                } else {
+                                    step++;
+                                    diffHeight--;
+                                    autoScaleResult()
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            sendWidth = width - diffWidth;
+            sendDepth = depth - diffDepth;
+            sendHeight = height - diffHeight;
+
+
+
+            const sendData = `${sendDepth}, ${sendHeight}, ${sendWidth}` + data;
+            window.lotus.c(window.lotus.Jb(window.lotus.vn.F(sendData)));
+            lastSend = sendData;
+        }, 50)
+
+    }
+
+    function autoScaleResult() {
+        clearInterval(intervalId);
+        if (results.length) {
+            window.lotus.c(window.lotus.Jb(window.lotus.vn.F(results[results.length - 1].lastSend)));
+            applyResult(results[results.length - 1].result);
+        }
+        console.info('Last Send', results[results.length - 1].lastSend)
+        console.info('Result:', results)
+    }
+
 </script>
 
 
